8b83ac3139d7fa27373c8c4d9d8355571f8cf7eb
kutluduman <kutluduman@gmail.com>
2021-01-29 01:06:56 -0500

    Styling for home page

---
+++ src/styles/HomeStyle.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components'
+
+import { maxAppWidth, MediumGray } from '../library'
+
+
+export const AppContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+  background-color: ${MediumGray};
+`;
+
+export const Main = styled.div`
+  max-width: ${maxAppWidth}px;
+  margin-right: auto;
+  margin-left: auto;
+  width: 100%;
+`;

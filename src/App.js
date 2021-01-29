b376fcd0b1c9c7ed5f60dec4597f4a3028146943
kutluduman <kutluduman@gmail.com>
2021-01-29 03:26:45 -0500

    Configured the app file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
-import { React } from 'react'
+import { React, useState } from 'react'
 import {AppContainer, Main} from './styles/HomeStyle'
 import Search from './components/Search'
+import NominatedList from './components/NominatedList'
+import ResultsList from './components/ResultsList'
 
 function App() {
+  const [results, setResults] = useState([]);
+  const [nominations, setNominations] = useState([]);
+
+  const addResults = (results) => {
+    setResults(results)
+  }
+
+  const nominateMovie = (movie) => {
+    setNominations([...nominations, movie])
+  }
+
+  const unNominateMovie = (id) => {
+    setNominations(nominations.filter((movie) => movie.id !== id))
+  }
+
   return (
     <AppContainer>
         <Main>
-          <Search/>
+          <Search addResults={addResults}/>
+          <ResultsList nominateMovie={nominateMovie} results={results}/>
+          <NominatedList nominations={nominations} unNominateMovie={unNominateMovie}/>
         </Main>
     </AppContainer>
   )

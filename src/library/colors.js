947ec2c2282c757a5eb38044c5fd744bb4d13b8a
kutluduman <kutluduman@gmail.com>
2021-01-29 00:17:04 -0500

    Created library files

---
+++ src/library/colors.js
@@ -0,0 +1,4 @@
+export const MediumBlue = '#234e66';
+export const DarkBlue = '#033048';
+export const MediumGray = '#939493';
+export const Amber = '#968938';
---
+++ src/library/constants.js
@@ -0,0 +1,12 @@
+export const column = 80
+
+export const gutter = 16
+export const cardWidth = 100
+
+export const maxAppWidth = (cardWidth + gutter * 2) * 5
+
+export const omdb = {
+  HOSTNAME: 'https://www.omdbapi.com/',
+}
+
+export const nominationSlots = 5

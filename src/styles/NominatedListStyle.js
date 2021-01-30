664653ac55fe49fc34b0ccdfd9747db8fb8efc1e
kutluduman <kutluduman@gmail.com>
2021-01-29 23:12:08 -0500

    Created Empty slot styling

--- src/styles/NominatedListStyle.js
+++ src/styles/NominatedListStyle.js
@@ -1,12 +1,27 @@
-import styled from 'styled-components';
-
+import styled from 'styled-components'
+import { gutter, cardWidth } from '../library'
 
 export const StyledDiv = styled.div`
   display: flex;
   flex-wrap: wrap;
-`;
+`
 
 export const StyledH2 = styled.h2`
   font-family: 'Poiret One', cursive;
   color: white;
-`;
+  margin: 20px ${gutter}px;
+`
+
+export const StyledEmptySlot = styled.div`
+  height: 150px;
+  width: ${cardWidth}px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: white;
+  background-color: #1c3f52;
+  margin: 5px ${gutter}px;
+  border-radius: 2px;
+  font-size: 40px;
+  font-family: 'Poiret One', cursive;
+`

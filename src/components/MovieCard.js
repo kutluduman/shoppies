f26643ecb88a9187daeadeb52ef750859c31b952
kutluduman <kutluduman@gmail.com>
2021-01-29 23:40:05 -0500

    Implemented stylings into the movie card component

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,17 +1,25 @@
 import React from 'react'
-import { StyledCard, StyledText, StyledIcon } from '../styles/MovieCardStyle'
+import {
+  StyledCard,
+  StyledYear,
+  StyledTitle,
+  StyledButton,
+} from '../styles/MovieCardStyle'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faAward } from '@fortawesome/free-solid-svg-icons'
 
 const MovieCard = ({ movie, handleNomination, nominated, disabled }) => {
   return (
     <StyledCard url={movie.Poster} nominated={nominated} disabled={disabled}>
-      <StyledIcon
-        icon={faAward}
+      <StyledButton
+        type="button"
         onClick={() => handleNomination(movie, nominated)}
         nominated={nominated}
-      />
-      <StyledText title={true}>{movie.Title}</StyledText>
-      <StyledText>{movie.Year}</StyledText>
+      >
+        <FontAwesomeIcon icon={faAward} />
+      </StyledButton>
+      <StyledTitle>{movie.Title}</StyledTitle>
+      <StyledYear>{movie.Year}</StyledYear>
     </StyledCard>
   )
 }
--- src/styles/MovieCardStyle.js
+++ src/styles/MovieCardStyle.js
@@ -1,12 +1,11 @@
-import styled from 'styled-components';
-import {cardWidth, gutter} from '../library';
-
-
+import styled from 'styled-components'
+import { cardWidth, gutter } from '../library'
 
 export const StyledCard = styled.div`
   height: 150px;
   width: ${cardWidth}px;
-  background: ${props => `linear-gradient(to bottom, transparent 0%, black 100%), url(${props.url})`};
+  background: ${(props) =>
+    `linear-gradient(to bottom, transparent 0%, black 100%), url(${props.url})`};
   background-size: cover;
   display: flex;
   flex-direction: column;
@@ -15,18 +14,25 @@ export const StyledCard = styled.div`
   margin: 5px ${gutter}px;
   transition: box-shadow 0.3s, border-color 0.3s, -webkit-box-shadow 0.3s;
   :hover {
-    box-shadow: 0 1px 2px -2px rgba(0, 0, 0, 0.16), 0 3px 6px 0 rgba(0, 0, 0, 0.12), 0 5px 12px 4px rgba(0, 0, 0, 0.09);
-  };
-  opacity: ${props => props.disabled ? '0.5' : '1'};
-`;
-
+    box-shadow: 0 1px 2px -2px rgba(0, 0, 0, 0.16),
+      0 3px 6px 0 rgba(0, 0, 0, 0.12), 0 5px 12px 4px rgba(0, 0, 0, 0.09);
+  }
+  opacity: ${(props) => (props.disabled ? '0.5' : '1')};
+`
 
 export const StyledTitle = styled.p`
+  margin: 0.2rem;
+  font-size: 18px;
+  text-align: center;
+  color: white;
+`
+
+export const StyledYear = styled.p`
   color: white;
   font-size: 13px;
   text-align: center;
   margin: 0.5rem;
-`;
+`
 
 export const StyledButton = styled.button`
   background-color: transparent;
@@ -37,5 +43,5 @@ export const StyledButton = styled.button`
   right: -15px;
   margin: 10px;
   font-size: 25px;
-  color:${props => props.nominated ? '#ffd43b' : 'white'};
-`;
+  color: ${(props) => (props.nominated ? '#ffd43b' : 'white')};
+`

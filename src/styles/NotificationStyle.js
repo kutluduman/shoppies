de4e9634f2799ec7c8073f4b3385e3584eea092a
kutluduman <kutluduman@gmail.com>
2021-01-29 23:07:14 -0500

    Notification styling was created

---
+++ src/styles/NotificationStyle.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export const StyledDiv = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: white;
+  position: absolute;
+  right: 50px;
+`

8f7ec4d7ead135aa5451bf1e858a0a62ea830a58
kutluduman <kutluduman@gmail.com>
2021-01-29 22:29:39 -0500

    Setting search fix on search component

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,45 +1,45 @@
-import React, {useState, useEffect } from 'react';
-import axios from 'axios';
-import {omdb} from '../library';
-import useDebounce from '../hooks/useDebounce';
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import { omdb } from '../library'
+import useDebounce from '../hooks/useDebounce'
 import {
   StyledSearchWrap,
   StyledIcon,
   StyledSearch,
-} from '../styles/SearchStyle';
-import { faSearch, faSpinner } from '@fortawesome/free-solid-svg-icons';
+} from '../styles/SearchStyle'
+import { faSearch, faSpinner } from '@fortawesome/free-solid-svg-icons'
 
-
-const Search = ({addResults}) => {
+const Search = ({ addResults }) => {
   // eslint-disable-next-line no-unused-vars
   const [search, setSearch] = useState(false)
   const [input, setInput] = useState('')
-  const debouncedSearchTerm = useDebounce(input,500);
-  const [message, setMessage] = useState(null);
+  const debouncedSearchTerm = useDebounce(input, 500)
+  const [message, setMessage] = useState(null)
 
   useEffect(() => {
-    if(debouncedSearchTerm) {
-      const searchUrl = `${omdb.HOSTNAME}?apikey=${process.env.REACT_APP_OMDB_API_KEY}&s=${debouncedSearchTerm}&r=json`;
-      
-      axios.get(searchUrl)
-      .then((res) => {
-        if(res.data.Response === 'True') {
-          addResults(res.data.Search);
-        } else {
-          setMessage(res.data.Error);
-          addResults([]);
-        }
-      })
-      .catch(() => {
-        setMessage('An unexpected error occured.');
-        setSearch(false);
-      })
-    }
-  },[debouncedSearchTerm,addResults]);
+    if (debouncedSearchTerm) {
+      const searchUrl = `${omdb.HOSTNAME}?apikey=${process.env.REACT_APP_OMDB_API_KEY}&s=${debouncedSearchTerm}&r=json`
 
+      setSearch(true)
+      axios
+        .get(searchUrl)
+        .then((res) => {
+          if (res.data.Response === 'True') {
+            addResults(res.data.Search)
+          } else {
+            setMessage(res.data.Error)
+            addResults([])
+          }
+        })
+        .catch(() => {
+          setMessage('An unexpected error occured.')
+          setSearch(false)
+        })
+    }
+  }, [debouncedSearchTerm, addResults])
 
   const handleChangeInput = (value) => {
-    setMessage(null);
+    setMessage(null)
     setInput(value)
   }
 
@@ -55,9 +55,9 @@ const Search = ({addResults}) => {
           onChange={(e) => handleChangeInput(e.target.value)}
         />
       </StyledSearchWrap>
-        {message && <p>{message}</p>}
-    </section>   
+      {message && <p>{message}</p>}
+    </section>
   )
 }
 
-export default Search;
+export default Search

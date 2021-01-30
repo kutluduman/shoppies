63fea3e1bab954756d05e2806961b531a3298fcc
kutluduman <kutluduman@gmail.com>
2021-01-29 23:21:45 -0500

    Created styling for Empty state

--- src/styles/ResultsListStyle.js
+++ src/styles/ResultsListStyle.js
@@ -1,9 +1,19 @@
-import styled from 'styled-components';
-
+import styled from 'styled-components'
 
 export const StyledSection = styled.section`
   display: flex;
   flex-wrap: wrap;
   margin-top: 15px;
   min-height: 320px;
-`;
+  justify-content: center;
+`
+
+export const StyledEmptyState = styled.div`
+  color: white;
+  font-family: 'Poiret One', cursive;
+  font-size: 25px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`

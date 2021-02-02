7a47e30180d06363e9438ed7fa73b2caa5173174
kutluduman <kutluduman@gmail.com>
2021-02-01 22:35:42 -0500

    Added logic for showing the results list with useState

--- src/components/ResultsList.js
+++ src/components/ResultsList.js
@@ -1,28 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFilm } from '@fortawesome/free-solid-svg-icons';
 import {
   StyledSection,
   StyledEmptyState,
   StyledMessage,
+  StyledButton,
 } from '../styles/ResultsListStyle';
 import MovieCard from './MovieCard';
 
-
 const ResultsList = ({ results, handleNomination, nominatedIds, message }) => {
+  const [showAll, setShowAll] = useState(false);
   return (
-    <StyledSection>
+    <StyledSection showAll={showAll}>
       {message && <StyledMessage>{message}</StyledMessage>}
       {results.length > 0 ? (
-        results.map((movie) => (
-          <MovieCard
-            key={movie.imdbID}
-            movie={movie}
-            handleNomination={handleNomination}
-            nominated={nominatedIds.has(movie.imdbID)}
-            disabled={nominatedIds.has(movie.imdbID)}
-          />
-        ))
+        <>
+          {results.map((movie) => (
+            <MovieCard
+              key={movie.imdbID}
+              movie={movie}
+              handleNomination={handleNomination}
+              disabled={nominatedIds.has(movie.imdbID)}
+            />
+          ))}
+          {!showAll ? (
+            <StyledButton
+              showAll={showAll}
+              onClick={() => setShowAll(true)}
+              type="button"
+            >
+              Show All
+            </StyledButton>
+          ) : null}
+        </>
       ) : (
         <StyledEmptyState>
           <FontAwesomeIcon icon={faFilm} />
--- src/styles/ResultsListStyle.js
+++ src/styles/ResultsListStyle.js
@@ -10,6 +10,11 @@ export const StyledSection = styled.section`
   position: relative;
   justify-content: space-between;
   flex-grow: 1;
+  @media (max-width: 750px) {
+    height: ${(props) => (props.showAll ? '100%' : '320px')};
+    overflow: hidden;
+    max-height: 100%;
+  }
 `;
 
 export const StyledEmptyState = styled.div`
@@ -31,4 +36,26 @@ export const StyledMessage = styled.p`
   right: 0;
   margin: 0 auto;
   text-align: center;
+`;
+
+export const StyledButton = styled.button`
+  display: none;
+  @media (max-width: 750px) {
+    display: ${(props) => (props.showAll ? 'none' : 'inline-block')};
+    border: none;
+    position: absolute;
+    width: 120px;
+    bottom: 5px;
+    color: white;
+    background: #8e6210;
+    border-radius: 2px;
+    color: white;
+    padding: 10px 30px;
+    z-index: 3;
+    left: 50%;
+    margin-left: -60px;
+    box-shadow: 0 1px 2px -2px rgba(0, 0, 0, 0.16),
+      0 3px 6px 0 rgba(0, 0, 0, 0.12), 0 5px 12px 4px rgba(0, 0, 0, 0.09);
+    font-size: 15px;
+  }
 `;
